2ee855a6fc32fb1830943dd836e023aa5ca73ba4
VasileS <VasileS@gilat.com>
2024-06-05 10:13:36 +0300

    Initial Commit

--- ahooServer/src/main/java/student/examples/ServerApp.java
+++ ahooServer/src/main/java/student/examples/ServerApp.java
@@ -10,19 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 public class ServerApp {
-    private static Player playerOne;
-    private static Player playerTwo;
-
     public static void main(String[] args) throws Exception {
-        List<Player> players = new ArrayList<>();
-        players.add(new Player("Jimmy",100));
-        players.add(new Player("Jimmy1",1000));
-        players.add(new Player("Jimmy2",10000));
 
-        players.forEach(ServerApp::printInBrackets);
-    }
-
-    public static void printInBrackets(Player player){
-        System.out.println("["+player+"]");
     }
 }
